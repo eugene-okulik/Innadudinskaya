7348d1b0ed1aebdd6f23b4ff520d8a06b088f664
InnaDudinskaya <inna.dudinskaya@gmail.com>
2026-04-08 21:22:18 +0300

    Homework_lesson 25 updated

--- homework/inna_dudinskaya/Homework_25/Task3_2.py
+++ homework/inna_dudinskaya/Homework_25/Task3_2.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.fixture
 def driver():
     chrome_driver = webdriver.Chrome()
